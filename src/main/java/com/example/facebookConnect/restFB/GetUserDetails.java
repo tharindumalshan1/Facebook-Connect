18adb87c21abf46d927e1c0c1dee0a862fc5f9d5
tharindumalshan1 <malshanbasnayake@gmail.com>
2021-03-26 15:47:37 +0530

    Add loggers in java class

--- src/main/java/com/example/facebookConnect/restFB/GetUserDetails.java
+++ src/main/java/com/example/facebookConnect/restFB/GetUserDetails.java
@@ -6,16 +6,19 @@ import com.restfb.Parameter;
 import com.restfb.Version;
 import com.restfb.json.JsonObject;
 import com.restfb.types.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
 
 public class GetUserDetails {
+    private static final Logger logger = LogManager.getLogger(GetUserDetails.class);
 
     public UserProfile GetProfileinfor(String accesstoken) {
         UserProfile objUserProfile=new UserProfile();
         FacebookClient facebookClient = new DefaultFacebookClient(accesstoken, Version.VERSION_10_0);
         User user = facebookClient.fetchObject("me", User.class);
-        System.out.println("User name: " + user.getName());
+        logger.info("User name : {}", user.getName());
         objUserProfile.setUserName(user.getName());
         return objUserProfile;
     }
@@ -30,7 +33,7 @@ public class GetUserDetails {
             JsonObject fetchObjectsResults = facebookClient.fetchObjects(Arrays.asList("me", "123456789"),
                     JsonObject.class, Parameter.with("fields", "name,id,email,picture"));
 
-            System.out.println(fetchObjectsResults);
+            logger.info(fetchObjectsResults);
 
             String temp = fetchObjectsResults.toString();
             objUserProfile.setUserName(temp.substring(temp.indexOf("\"name\":\"")+8, temp.indexOf("\",\"id\":\"")));
@@ -38,7 +41,7 @@ public class GetUserDetails {
             objUserProfile.setEmail(temp.substring(temp.indexOf("\",\"email\"")+11, temp.indexOf("\",\"picture\":")));
             objUserProfile.setPicture(temp.substring(temp.indexOf("\"url\":\"")+7, temp.indexOf("\",\"width\"")));
         } catch (Exception e){
-            System.out.println("Error"+ e);
+            logger.error("Error", e);
         }
         return objUserProfile;
     }
